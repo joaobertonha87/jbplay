7082f665a567b5ff76baafb95976e93b0f20bde9
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-08-29 23:57:54 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='jb-play-v27-9-config-score-names-20260830';
+const CACHE='jb-play-v28-0-editnames-20260830';
 const ASSETS=['./','./index.html','./treino.html','./scout.html','./jb-logo-lime.png','./jb-watermark-lime.png','./jb-hero-beach.jpg','./jb-3d-reference.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
