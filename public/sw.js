0fc8c14336868300e9f37946ba79482b81c3a8b0
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-09-01 11:11:59 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='jb-play-aula-inteligente-v33-20260901';
-const CORE=['./index.html?v=3300','./treino.html?v=3300','./scout.html?v=3300','./manifest.webmanifest?v=3300'];
+const CACHE='jb-play-aula-na-mao-v34-20260901';
+const CORE=['./index.html?v=3400','./treino.html?v=3400','./scout.html?v=3400','./manifest.webmanifest?v=3400'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
 });
@@ -11,7 +11,7 @@ self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url);
   const isApp=/\.(?:html|js|webmanifest)$/i.test(url.pathname)||url.pathname.endsWith('/');
   if(isApp){
-    event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html?v=3300'))));
+    event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html?v=3400'))));
     return;
   }
   event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{
